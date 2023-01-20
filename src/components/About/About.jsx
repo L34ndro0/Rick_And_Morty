7742c5fa1e08dfe64ff56f80ee2058ca147a5282
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-01-20 18:26:15 -0300

    actualizacion

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,8 @@ import style from './About.module.css'
 export default function About(){
     return (
         <div className={style.contenedor} >
-            <h1 className={style.titulo} >Acerca del creador</h1>
+            <h1 className={style.titulo} >Acerca del creador:</h1>
+            <span className={style.descripcion} >Creado por Leandro Carrizo</span>
         </div>
     )
 }
